565787bfd8f0b58c08820efeea297374ec4254f8
UB <yyb2535@gmail.com>
2020-07-20 08:46:14 +0900

    push

--- src/components/Hook.js
+++ src/components/Hook.js
@@ -1,26 +1,55 @@
 import React, {useReducer, useState} from "react";
-import {useDispatch, useSelector, useStore} from "react-redux";
+// import axios from "axios";
+// import {useDispatch, useSelector, useStore} from "react-redux";
 
-function reducer(__state, action) {
+function reducer(state, action) {
   switch (action.type) {
     case "INCREMENT":
-      return __state + 1;
+      return state + 1;
     case "DECREMENT":
-      return __state - 1;
+      return state - 1;
     case "GET_NUM":
-      return makeNum(__state);
+      console.log(state)
+      return state;
+    case "DELETE":
+      return state = 0;
     default:
-      return __state;
+      return state;
   }
 }
 
 function Hook() {
   const [count, setCount] = useState(0);
-  const [_state, _dispatch] = useReducer(reducer, initState);
+  const [number, dispatch] = useReducer(reducer, 0);
+
+  const onIncrease = () => {
+    dispatch({
+      type: "INCREMENT"
+    })
+  };
+  const onDecrease = () => {
+    dispatch({
+      type: "DECREMENT"
+    })
+  };
+  const onGet = () => {
+    dispatch({
+      type: "GET_NUM"
+    })
+  };
+  const onDelete = () => {
+    dispatch({
+      type: "DELETE"
+    })
+  };
   return (
     <>
       {count}
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      {number}
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+      <button onClick={onGet}>읽기</button>
+      <button onClick={onDelete}>초기화</button>
     </>
   );
 }
--- src/routes/MyHome.js
+++ src/routes/MyHome.js
@@ -2,11 +2,14 @@ import React from "react";
 // import {useDispatche, useSelector} from "react-redux";
 import SkillContainer from "../components/SkillContainer";
 import ProjectContainer from "../components/ProjectContainer";
+// import Hook from "../components/Hook";
+import ReducerTest from "../components/ReducerTest";
 import "./MyHome.css";
 
 function MyHome() {
   return (
     <main>
+      <ReducerTest />
       <SkillContainer />
       <ProjectContainer />
     </main>
